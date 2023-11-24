de911342d98a6e293e9db451d3c333b378a18f8b
Artem Gordash <87481496+artemgordash@users.noreply.github.com>
2023-11-25 00:25:45 +0200

    Fix adb installation command and add localization support

--- adb-install.ps1
+++ adb-install.ps1
@@ -1,3 +1,3 @@
 flutter build apk --release --no-tree-shake-icons
 cd C:\Users\arten\platform-tools
-.\adb -s R9DW60382TN install C:\Projects\smartcity\build\app\outputs\flutter-apk\app-release.apk
+.\adb -s R9DW60382TN install C:\Projects\smartcity\build\app\outputs\flutter-apk\app-release.apk 
--- lib/controller.dart
+++ lib/controller.dart
@@ -1,8 +1,23 @@
 import 'dart:ui';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class GlobalController extends GetxController {
   RxBool isLoading = false.obs;
   Rx<Locale> locale = const Locale('ro').obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    final storage = GetStorage();
+    ever(locale, (_) {
+      storage.write('locale', locale.value.languageCode);
+    });
+    final localeFromStorage = storage.read('locale');
+
+    if (localeFromStorage != null) {
+      locale.value = Locale(localeFromStorage);
+    }
+  }
 }
