747902c32aafd888a8d9f3df32caad6a679cb123
RobertoMMM <cataraga.robert@gmail.com>
2024-07-08 14:24:23 +0300

    changes

--- neuron/admin-panel/src/app/login/page.tsx
+++ neuron/admin-panel/src/app/login/page.tsx
@@ -1,20 +1,13 @@
 import { AuthPage } from "@components/auth-page";
 import { authProviderServer } from "@providers/auth-provider";
 import { redirect } from "next/navigation";
-import { NewsBot } from "../../../../news-bot/bot";
 
 export default async function Login() {
   const data = await getData();
 
-  // if (data.authenticated) {
-  //   redirect(data?.redirectTo || "/");
-  // }
-
-  NewsBot.instance.on("custom", (resp) => {
-    console.log(resp);
-  });
-
-  NewsBot.instance.emit("start");
+  if (data.authenticated) {
+    redirect(data?.redirectTo || "/");
+  }
 
   return <AuthPage type="login" />;
 }
